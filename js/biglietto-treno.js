eda3614c8083dfe6285f5babc3b3bcc9828fd140
Simone <simonelega.sl@gmail.com>
2019-12-18 15:15:35 +0100

    applicato sconto del 40% per gli over 65

--- js/biglietto-treno.js
+++ js/biglietto-treno.js
@@ -13,12 +13,16 @@ console.log(eta);
 // Il prezzo del biglietto è definito in base ai km (0.21 € al km)
 var prezzoBase = 0.21;
 var prezzoTratta= prezzoBase * chilometri;
+var euro = " €";
 
 // va applicato uno sconto del 20% per i minorenni e del 40% per gli over 65.
 if (eta < 18) {
   var prezzoTratta20 = prezzoTratta - prezzoTratta * (20 / 100);
-  console.log(prezzoTratta20.toFixed(2) + " €");
-} else {
+  console.log(prezzoTratta20.toFixed(2) + euro);
+} else if (eta > 65) {
+  var prezzoTratta40 = prezzoTratta - prezzoTratta * (40 / 100);
+  console.log(prezzoTratta40.toFixed(2) + euro);
+} else{
   prezzoTratta;
-  console.log(prezzoTratta);
+  console.log(prezzoTratta + euro);
 }
