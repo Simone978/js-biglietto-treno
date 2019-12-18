86cc906247e030ee0c19ade5a5bc32474cc000f8
Simone <simonelega.sl@gmail.com>
2019-12-18 15:11:38 +0100

    applicato uno sconto del 20% per i minorenni

--- js/biglietto-treno.js
+++ js/biglietto-treno.js
@@ -13,3 +13,12 @@ console.log(eta);
 // Il prezzo del biglietto è definito in base ai km (0.21 € al km)
 var prezzoBase = 0.21;
 var prezzoTratta= prezzoBase * chilometri;
+
+// va applicato uno sconto del 20% per i minorenni e del 40% per gli over 65.
+if (eta < 18) {
+  var prezzoTratta20 = prezzoTratta - prezzoTratta * (20 / 100);
+  console.log(prezzoTratta20.toFixed(2) + " €");
+} else {
+  prezzoTratta;
+  console.log(prezzoTratta);
+}
