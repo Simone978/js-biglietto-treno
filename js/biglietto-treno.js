1ba6dbbf851def38a3ee8d2ea190321303cde783
Simone <simonelega.sl@gmail.com>
2019-12-18 15:33:39 +0100

    inserito prezzo biglietto nell'html

--- js/biglietto-treno.js
+++ js/biglietto-treno.js
@@ -15,14 +15,19 @@ var prezzoBase = 0.21;
 var prezzoTratta= prezzoBase * chilometri;
 var euro = " €";
 
+// controllo che le risposte siano inserite in maniera corretta
+if( isNaN(chilometri) || isNaN(eta)) {
+  document.getElementById("avvertenza").innerHTML= "Attenzione, non hai inserito le risposte in modo corretto";
+} else {
 // va applicato uno sconto del 20% per i minorenni e del 40% per gli over 65.
 if (eta < 18) {
   var prezzoTratta20 = prezzoTratta - prezzoTratta * (20 / 100);
-  console.log(prezzoTratta20.toFixed(2) + euro);
+  document.getElementById('prezzo').innerHTML = "Il tuo biglietto costa " + prezzoTratta20.toFixed(2) + euro;
 } else if (eta > 65) {
   var prezzoTratta40 = prezzoTratta - prezzoTratta * (40 / 100);
-  console.log(prezzoTratta40.toFixed(2) + euro);
+    document.getElementById('prezzo').innerHTML = "Il tuo biglietto costa " + prezzoTratta40.toFixed(2) + euro;
 } else{
   prezzoTratta;
-  console.log(prezzoTratta + euro);
+    document.getElementById('prezzo').innerHTML = "Il tuo biglietto costa " + prezzoTratta + euro;
+}
 }
